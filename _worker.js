a20d1ba808014350e945c47f9dbf517897867d09
jaraim <117310975+jaraim@users.noreply.github.com>
2024-01-21 01:41:48 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
   async fetch(request, env) {
     let url = new URL(request.url);
     if (url.pathname.startsWith('/')) {
-      url.hostname = 'www.wupg.eu.org'
+      url.hostname = 'ph.jaraim.top'
       let new_request = new Request(url, request);
       return fetch(new_request);
     }
